2eefa4a36d9a2272e04d78bb692d2db1a404b49d
luyoulun <luyoulun@wxchina.com>
2017-12-26 23:56:00 +0800

    '123'

--- app/src/main/java/com/amberllo/mdm/fragment/HostFragment.java
+++ app/src/main/java/com/amberllo/mdm/fragment/HostFragment.java
@@ -8,10 +8,12 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import com.amberllo.mdm.R;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -31,9 +33,11 @@ public class HostFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
         listView = (ListView) view.findViewById(R.id.applist);
         PackageManager pm = getContext().getPackageManager();
+        List<String> applist = new ArrayList<>();
         List<ApplicationInfo> applicationInfoList = pm.getInstalledApplications(PackageManager.GET_UNINSTALLED_PACKAGES);
         for(ApplicationInfo app:applicationInfoList){
-            
+            applist.add(app.packageName);
         }
+        listView.setAdapter(new ArrayAdapter<String>(getContext(),android.R.layout.simple_list_item_1,applist));
     }
 }
